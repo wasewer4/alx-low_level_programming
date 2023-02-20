054ede3680536fae523a63fb90a8404a3078a523
wasewer4 <wasewer2016@gmail.com>
2023-02-20 15:01:41 -0800

    fixed errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
