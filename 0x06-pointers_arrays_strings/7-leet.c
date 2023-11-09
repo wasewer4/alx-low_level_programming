2a26f0d6aa3a9a35d928f924efb6268682891ea2
Hakime <wasewer2016@gmail.com>
2023-11-09 22:48:26 +0100

    err

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -26,19 +26,3 @@ char *leet(char *s)
 
 	return (s);
 }
-
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
-
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
-    return (0);
-}
