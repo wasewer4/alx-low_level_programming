db27df14dbff149fed911ff9343baf126b810277
Hakime <wasewer2016@gmail.com>
2023-11-16 01:28:27 +0100

    ex04

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * 
+ * _pow_recursion - raise base integer to the power of following integer
+ *
+ * @x: base number
+ * @y: power
+ *
+ * Return: result of computation
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else if (y == 0)
+		return (1);
+
+	else
+		return (-1);
+
+	return (0);
+}
