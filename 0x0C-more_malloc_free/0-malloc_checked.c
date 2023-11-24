5882e490c47a83f58104ec4c5d5973ac41b99f89
Hakime <wasewer2016@gmail.com>
2023-11-24 02:10:30 +0100

    cc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *pt;
 
-	ptr = malloc(b);
+	pt = malloc(b);
 
-	if (ptr == NULL)
+	if (pt == NULL)
 		exit(98);
 
-	return (ptr);
+	return (pt);
 }
